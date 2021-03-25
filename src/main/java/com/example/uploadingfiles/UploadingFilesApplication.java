3ac0e6079359a3c4315a4e28f14863fd2b74bf9a
N1tr0N1x <nizar.zitouni@univ-constantine2.dz>
2021-03-25 22:39:39 +0100

    Initial commit

---
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,29 @@
+package com.example.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import com.example.uploadingfiles.storage.StorageProperties;
+import com.example.uploadingfiles.storage.StorageService;
+
+@EnableEurekaClient
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
+}
